9eec342ee41316c04f55c0aeeb3e737576a18c2a
JOHS\zlwlw <johs970917@gmail.com>
2024-04-29 23:02:30 +0900

    배경 색상과 글자 색상 변경해보기

--- exam/lib/main.dart
+++ exam/lib/main.dart
@@ -3,10 +3,15 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Center(
           child: Text(
             'Code Factory',
+            style: TextStyle(
+              color: Colors.white,
+            ),
           ),
         ),
       ),
