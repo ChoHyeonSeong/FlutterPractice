d3f2a322b18713d7b8c28b45816d325526cfb297
JOHS\zlwlw <johs970917@gmail.com>
2024-05-05 19:19:16 +0900

    Life Cycle 실습1

--- statelesswidget_lifecycle/lib/screen/home_screen.dart
+++ statelesswidget_lifecycle/lib/screen/home_screen.dart
@@ -35,11 +35,45 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 }
 
-class WasabiWidget extends StatelessWidget {
-  const WasabiWidget({super.key});
+class WasabiWidget extends StatefulWidget {
+  WasabiWidget({super.key}){
+    print('1) Stateful Widget constructor');
+  }
 
   @override
+  State<WasabiWidget> createState() {
+    print('2) Stateful Widget createState');
+    return _WasabiWidgetState();
+  }
+}
+
+class _WasabiWidgetState extends State<WasabiWidget> {
+  @override
+  void initState() {
+    print('3) Stateful Widget initState');
+    // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    print('4) Stateful Widget didChangeDependencies');
+    super.didChangeDependencies();
+  }
+  @override
   Widget build(BuildContext context) {
+    print('5) Stateful Widget build');
     return Container(width: 50.0, height: 50.0, color: Colors.lightGreen);
+  }
+  @override
+  void deactivate() {
+    print('6) Stateful Widget deactivate');
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print('7) Stateful Widget dispose');
+    super.dispose();
   }
 }
