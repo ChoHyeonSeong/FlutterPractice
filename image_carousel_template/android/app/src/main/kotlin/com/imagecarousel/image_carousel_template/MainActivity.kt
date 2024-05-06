07782bbf087c3b4d54b264a993d6779dc57c1dd5
JOHS\zlwlw <johs970917@gmail.com>
2024-05-06 22:44:16 +0900

    이미지 렌더링

---
+++ image_carousel_template/android/app/src/main/kotlin/com/imagecarousel/image_carousel_template/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.imagecarousel.image_carousel_template
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ image_carousel_template/lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:image_carousel_template/screen/home_screen.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen(),
+    ),
+  );
+}
